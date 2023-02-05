362731b55e92ddc81ef7fa742c07c6fad1abe33b
Victor Souza <70328144+victorvssouza@users.noreply.github.com>
2023-02-05 15:11:30 -0300

    Filtrando com expressão regular

--- introducao-javascript/js/filtra.js
+++ introducao-javascript/js/filtra.js
@@ -4,12 +4,13 @@ campoFiltro.addEventListener("input", function(){
     console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
 
-    if (this.value.length > 0){
+    if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++){
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
-            if (nome != this.value){
+            var expressao = new RegExp(this.value, "i"); 
+            if (!expressao.test(nome)) {
                 paciente.classList.add("invisivel");
             } else {
                 paciente.classList.remove("invisivel");
