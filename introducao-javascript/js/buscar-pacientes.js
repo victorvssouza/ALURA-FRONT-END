42f923fc86eaa9315b693488eb1cf1fb58dd8e2a
Victor Souza <70328144+victorvssouza@users.noreply.github.com>
2023-02-05 19:56:01 -0300

    Introdução ao AJAX

---
+++ introducao-javascript/js/buscar-pacientes.js
@@ -0,0 +1,13 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+
+    xhr.addEventListener("load", function() {
+        console.log(xhr.responseText);
+    });
+
+    xhr.send();
+});
