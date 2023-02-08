0281e2bbf5444d7a421734fce24b3639ba659104
Victor Souza <vs.developer.vs310@gmail.com>
2023-02-08 14:23:25 -0300

    Comportamentos de classes

--- POO-js/index.js
+++ POO-js/index.js
@@ -1,24 +1,36 @@
 class Cliente {
     nome;
     cpf;
-    rg;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = "12345678900";
-cliente1.rg = "1234560"
-cliente1.agencia = "1001";
-cliente1.saldo = "0";
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "12345678901";
-cliente2.rg = "1234561"
-cliente2.agencia = "1001";
-cliente2.saldo = "0";
 
-console.log(cliente1, cliente2);
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.saldo = 100;
+console.log(contaCorrenteRicardo.saldo);
+contaCorrenteRicardo.sacar(500);
+
+console.log(contaCorrenteRicardo.saldo);
+console.log(cliente1);
+console.log(cliente2);
