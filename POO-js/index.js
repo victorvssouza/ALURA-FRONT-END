d2d4f7fe216dfd6c144952e6845106e98041c0a6
Victor Souza <vs.developer.vs310@gmail.com>
2023-02-15 14:28:36 -0300

    Composição de classes

--- POO-js/index.js
+++ POO-js/index.js
@@ -1,43 +1,34 @@
-class Cliente {
-    nome;
-    cpf;
-}
+import {Cliente} from "../POO-js/Cliente.js";
+import {ContaCorrente} from "../POO-js/ContaCorrente.js";
 
-class ContaCorrente {
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if (this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if (valor <= 0){
-           return;
-        }
-        this._saldo += valor;
-    }
-}
-
+//CLIENTE 1
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = "12345678900";
 
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1;
+contaCorrenteRicardo.depositar(500);
+
+// contaCorrenteRicardo.depositar(100);
+// contaCorrenteRicardo.depositar(100);
+// contaCorrenteRicardo.depositar(-100);
+// const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo)
+
+//CLIENTE 2
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = "12345678901";
 
-const contaCorrenteRicardo = new ContaCorrente();
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-contaCorrenteRicardo.agencia = 1001;
+console.log(conta2);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-const valorSacado = contaCorrenteRicardo.sacar(50);
-
-console.log(valorSacado);
-console.log(contaCorrenteRicardo)
+contaCorrenteRicardo.transferir(200, conta2);
+console.log(conta2);
+console.log(contaCorrenteRicardo);
