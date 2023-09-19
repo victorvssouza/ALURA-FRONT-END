1ae644f8b71081b1a16c6cac9d029e97fda95d84
unknown <Administrador@UNI001_L005_003.serlab.intra>
2023-09-19 19:42:45 -0300

    logica ex2 - concluido

--- Logica de Programação/logica-js-projeto_inicial/ex2.js
+++ Logica de Programação/logica-js-projeto_inicial/ex2.js
@@ -1,7 +1,28 @@
 // Crie um contador que comece em 1 e vá até 10 usando um loop while. Mostre cada número.
+// var cont = 0;
+// while (cont <= 10 ){
+//     console.log(cont);
+//     cont++;
+// }
 
 // Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+// var cont = 10;
+// while (cont >= 0 ){
+//     console.log(cont);
+//     cont--;
+// }
 
-// Crie um programa de contagem regressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// Crie um programa de contagem regressiva. Peça um número e conte dele até 0, usando um loop while no console do navegador.
+// var cont = prompt('Digite um número para executar a contagem regressiva: ');
+// while (cont >= 0){
+//     console.log(cont)
+//     cont--;
+// }
 
 // Crie um programa de contagem progressiva. Peça um número e conte de 0 até esse número, usando um loop while no console do navegador.
+// var cont = prompt('Digite um número para executar a contagem regressiva: ');
+// var i = 0;
+// while (i <= cont){
+//     console.log(i)
+//     i++;
+// }
