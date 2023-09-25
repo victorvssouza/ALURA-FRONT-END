6c77d40f30eb1a819b563ea8c25dd4bf5d9e0938
victorvssouza <vs.developer.vs310@gmail.com>
2023-09-25 14:01:25 -0300

    Uso da função Math.radom

--- Logica de Programação/logica-js-projeto_inicial/app.js
+++ Logica de Programação/logica-js-projeto_inicial/app.js
@@ -5,7 +5,7 @@ let chute;
 let tentativas = 1;
 
 //Definição do numero secreto e comparação
-let numeroSecreto = 29;
+let numeroSecreto = parseInt(Math.random() * 100   + 1);
 
 //enquanto chute for diferente de numeroSecreto
 while (chute != numeroSecreto) {
@@ -23,6 +23,7 @@ while (chute != numeroSecreto) {
     }
 }
 
+//Simplificando a condicional com operador ternário 
 let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 alert(`Acertou!!! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
 
